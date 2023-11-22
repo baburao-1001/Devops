773ea8c01248cbf097ce7fd6db090a97a9aaf78d
baburao-1001 <151762062+baburao-1001@users.noreply.github.com>
2023-11-22 21:16:30 +0530

    Create deploy.py
    Deploy

---
+++ deploy.py
@@ -0,0 +1,13 @@
+import time
+
+def deploy_application(environment, version):
+    # ... (same content as before)
+
+def run_tests(environment):
+    # ... (same content as before)
+
+def main():
+    # ... (same content as before)
+
+if __name__ == "__main__":
+    main()
